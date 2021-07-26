f6a9611ca1c7072613acdc9e4d50e8b1d54a8a4e
Mohanad Elsafty <mohan3d94@gmail.com>
2021-07-26 17:19:29 +0800

    chor: add comparison and testing for providers

--- main.py
+++ main.py
@@ -49,16 +49,23 @@ if __name__ == '__main__':
     #     except Exception as e:
     #         print("{}: {}".format(ticker, e))
 
-    ticker = 'NVDA'
-    ticker = 'THO'
+    # ticker = 'NVDA'
+    # ticker = 'THO'
+    ticker = 'MSFT'
     p = get_stock_info_provider(ticker)
     a = get_intrinsic_value_calculator(ticker, p)
     # print(a.operating_cash_flow_projected)
-    # print(a.intrinsic_value_before_cash_debt)
-    # print(a.less_debt_per_share)
-    # print(a.plus_cash_per_share)
-    # print(a.final_intrinsic_value_per_share)
-
+    print(a.intrinsic_value_before_cash_debt)
+    print(a.less_debt_per_share)
+    print(a.plus_cash_per_share)
+    print(a.final_intrinsic_value_per_share)
+    # print(p.beta)
+    # print(p.shares_outstanding)
+    # print(p.last_close)
+    # print(p.operating_cashflow)
+    # print(p.total_cash)
+    # print(p.total_debt)
+    # print(p.eps_next_5_years)
     # print(p.operating_cashflow)
 
     # import morningstar_stmt as ms
@@ -66,19 +73,38 @@ if __name__ == '__main__':
     #
     # ms.download_stmt(tl.all)
 
-    from client import MorningstarClient
+    # from client import MorningstarClient
+    #
+    # c = MorningstarClient('MSFT')
+    # cashflow = c.cash_flow()
+    # print(cashflow.loc['Cash Generated from Operating Activities'])
+    #
+    # balancesheet = c.balance_sheet(quarterly=True)
+    # print(balancesheet.loc['Cash, Cash Equivalents and Short Term Investments'])
+    #
+    # print(balancesheet.loc['Current Debt and Capital Lease Obligation'])
+    # print(balancesheet.loc['Long Term Debt'])
+    #
+    # mp = MorningstarProvider('MSFT')
+    # print(mp.operating_cashflow)
+    # print(mp.total_cash)
+    # print(mp.total_debt)
 
-    c = MorningstarClient('MSFT')
-    cashflow = c.cash_flow()
-    print(cashflow.loc['Cash Generated from Operating Activities'])
+    print('-' * 50)
+    print('-' * 50)
+    print('-' * 50)
 
-    balancesheet = c.balance_sheet(quarterly=True)
-    print(balancesheet.loc['Cash, Cash Equivalents and Short Term Investments'])
-
-    print(balancesheet.loc['Current Debt and Capital Lease Obligation'])
-    print(balancesheet.loc['Long Term Debt'])
-
-    mp = MorningstarProvider('MSFT')
-    print(mp.operating_cashflow)
-    print(mp.total_cash)
-    print(mp.total_debt)
+    ticker = 'MSFT'
+    p = MorningstarProvider(ticker)
+    a = get_intrinsic_value_calculator(ticker, p)
+    print(a.intrinsic_value_before_cash_debt)
+    print(a.less_debt_per_share)
+    print(a.plus_cash_per_share)
+    print(a.final_intrinsic_value_per_share)
+    # print(p.beta)
+    # print(p.shares_outstanding)
+    # print(p.last_close)
+    # print(p.operating_cashflow)
+    # print(p.total_cash)
+    # print(p.total_debt)
+    # print(p.eps_next_5_years)
